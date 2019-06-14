99be3c377f6ef076aeff8f1e784faf191ccf0b31
xutaotao <3451590500@qq.com>
2019-06-14 14:27:46 +0800

    1

--- src/app/index/controller/login.go
+++ src/app/index/controller/login.go
@@ -4,12 +4,17 @@ import (
 	"app/index/model"
 	"common"
 	"fmt"
+	"math"
 	"net/http"
+	"strconv"
+
+	//"strconv"
 )
 
-type data struct {
+type DATA struct {
 	List interface{} `json:"list"`
 	Count int `json:"count"`
+	Page int `json:"page"`
 }
 
 //获取用户信息返回给前端的数据字段
@@ -20,10 +25,18 @@ type uesrInfo struct {
 	CreateTime string `json:"create_time"`
 }
 
+type null struct {
+
+}
+
+type ONE struct {
+	Id string `json:"id"`
+	Rate string `json:"rate"`
+}
+
 //获取用户列表
 func UserList(w http.ResponseWriter, r *http.Request)  {
 
-	r.ParseForm()//解析url传递的参数，对于POST则解析响应包的主体（request body）
 	//注意:如果没有调用ParseForm方法，下面无法获取表单的数据
 	limit := common.STRINGTOINT(common.GETINIT(r,"limit","10"))
 	page := common.STRINGTOINT(common.GETINIT(r,"page","10"))
@@ -35,7 +48,7 @@ func UserList(w http.ResponseWriter, r *http.Request)  {
 
 	common.JSONTOSTRUCT(JsonOut,&u)
 
-	dd := data{u,1}
+	dd := DATA{u,1,0}
 
 	fmt.Fprintln(w, common.RETURNDATA(200,"成功",dd))
 
@@ -43,7 +56,7 @@ func UserList(w http.ResponseWriter, r *http.Request)  {
 
 //新增数据
 func AddData(w http.ResponseWriter, r *http.Request)  {
-	r.ParseForm()//解析url传递的参数，对于POST则解析响应包的主体（request body）
+
 	//注意:如果没有调用ParseForm方法，下面无法获取表单的数据
 	name := common.GETINIT(r,"name","")
 	age := common.STRINGTOINT(common.GETINIT(r,"age","10"))
@@ -59,6 +72,226 @@ func AddData(w http.ResponseWriter, r *http.Request)  {
 	}
 
 }
+
+func GetWord(w http.ResponseWriter, r *http.Request)  {
+
+	limit := common.STRINGTOINT(common.POSTINIT(r,"limit","10"))
+
+	var path = "C:\\Users\\PC\\Desktop\\allFile\\go\\apiFrame\\upload/word.txt"
+	loginModel := model.LoginConstruct()
+
+
+	data,_:= loginModel.GetFileContentAsStringLines(path)
+	s:= common.Random(data,limit)
+
+	dd := DATA{s,1,0}
+
+	fmt.Fprintln(w, common.RETURNDATA(200,"成功",dd))
+
+}
+
+func GetWordZ(w http.ResponseWriter, r *http.Request)  {
+
+	limit := common.STRINGTOINT(common.POSTINIT(r,"limit","5"))
+
+	var path = "C:\\Users\\PC\\Desktop\\allFile\\go\\apiFrame\\upload/word_z.txt"
+	loginModel := model.LoginConstruct()
+
+
+	data,_:= loginModel.GetFileContentAsStringLines(path)
+	s:= common.Random(data,limit)
+
+	dd := DATA{s,1,0}
+
+	fmt.Fprintln(w, common.RETURNDATA(200,"成功",dd))
+
+}
+
+/**********************************/
+func IsPlay(w http.ResponseWriter, r *http.Request)  {
+
+	type NUM struct {
+		Num string `json:"num"`
+	}
+	type COUNT struct {
+		Count int `json:"count"`
+	}
+
+	r.ParseForm()//解析url传递的参数，对于POST则解析响应包的主体（request body）
+
+	uid := common.STRINGTOINT(common.POSTINIT(r,"uid","0"))
+
+	loginModel := model.LoginConstruct()
+
+	JsonOut := loginModel.IsPlay(uid)
+
+	var num []NUM
+
+	flag := 0
+
+	common.JSONTOSTRUCT(JsonOut,&num)
+
+	if common.STRINGTOINT(num[0].Num) > 0{
+		flag = 1
+	}else {
+		flag = 0
+	}
+
+	c := COUNT{flag}
+
+	fmt.Fprintln(w, common.RETURNDATA(200,"成功",c))
+
+}
+//中奖用户
+func AllLuckList(w http.ResponseWriter, r *http.Request)  {
+	type LIST struct {
+		Ctime string `json:"ctime"`
+		Name string `json:"name"`
+		Type string `json:"type"`
+		NickName string `json:"nickName"`
+		HeadImage string `json:"headImage"`
+	}
+
+	type NUM struct {
+		Num string `json:"num"`
+	}
+	limit := common.STRINGTOINT(common.POSTINIT(r,"limit","10"))
+	page := common.STRINGTOINT(common.POSTINIT(r,"page","1"))
+
+	loginModel := model.LoginConstruct()
+
+	//获取指定数据列表
+	JsonOut := loginModel.AllLuckList(limit,page)
+	var list []LIST
+
+	common.JSONTOSTRUCT(JsonOut,&list)
+
+	//获取数量
+	JsonOutCount := loginModel.AllLuckListCount()
+	var Count []NUM
+	common.JSONTOSTRUCT(JsonOutCount,&Count)
+
+	count := common.STRINGTOINT(Count[0].Num)
+
+	//fmt.Fprintln(w, count/limit)
+
+
+	dd := DATA{list,count,common.FLOAT64TOINT(math.Ceil(common.INTTOFLOAT64(count/limit)+0.5))}
+
+	fmt.Fprintln(w, common.RETURNDATA(200,"成功",dd))
+
+
+}
+
+//获取当前用户游戏次数
+func GetGameTimes(w http.ResponseWriter, r *http.Request)  {
+	type NUM struct {
+		Num string `json:"num"`
+	}
+	uid := common.STRINGTOINT(common.POSTINIT(r,"uid","10"))
+	level := common.STRINGTOINT(common.POSTINIT(r,"level","1"))
+	className := GetClassName(level)
+
+	loginModel := model.LoginConstruct()
+
+	//获取指定数据列表
+	JsonOut := loginModel.GetGameTimes(className)
+
+	var list []NUM
+
+	common.JSONTOSTRUCT(JsonOut,&list)
+
+	allNum,_ := strconv.Atoi(list[0].Num)
+
+	//获取当前用户今天玩了多少次
+	JsonOut1 := loginModel.GetGameTimesToDay(uid)
+
+	var list1 []NUM
+
+	common.JSONTOSTRUCT(JsonOut1,&list1)
+
+	yNum,_ := strconv.Atoi(list1[0].Num)
+
+	dd := NUM{strconv.Itoa(allNum-yNum)}
+	fmt.Fprintln(w, common.RETURNDATA(200,"成功",dd))
+}
+
+func Luck(w http.ResponseWriter, r *http.Request)  {
+
+	type prize struct {
+		Id string `json:"id"`
+		Name string `json:"name"`
+		Num string `json:"num"`
+	}
+
+	//uid := common.STRINGTOINT(common.POSTINIT(r,"uid","10"))
+	loginModel := model.LoginConstruct()
+	//获取指定数据列表
+	JsonOut := loginModel.GetPrizeList()
+
+	var list []ONE
+
+	common.JSONTOSTRUCT(JsonOut,&list)
+
+	id := isLuck(list)
+	JsonOut1 := loginModel.GetPrizeById(id)
+
+	var prizes []prize
+
+	common.JSONTOSTRUCT(JsonOut1,&prizes)
+
+	fmt.Fprintln(w, common.RETURNDATA(200,"成功",prizes[0]))
+}
+
+//更改游戏状态
+func UpdateStatus(w http.ResponseWriter, r *http.Request)  {
+	goodId := common.STRINGTOINT(common.POSTINIT(r,"goodId","10"))
+	status := common.STRINGTOINT(common.POSTINIT(r,"status","1"))
+	loginModel := model.LoginConstruct()
+	//获取指定数据列表
+	loginModel.UpdateStatus(goodId,status)
+
+	var dd null
+
+	fmt.Fprintln(w, common.RETURNDATA(200,"成功",dd))
+}
+
+func GetClassName(level int) (string) {
+
+	if level > 80{
+		level = 70
+	}
+
+	classS := map[string]string{
+		"0":"oneClass",
+		"1":"twoClass",
+		"2":"threeClass",
+		"3":"fourClass",
+		"4":"fiveClass",
+		"5":"sixClass",
+		"6":"sevenClass",
+		"7":"eightClass",
+	}
+
+	return classS[strconv.Itoa(level/10)]
+}
+
+//判读是否中奖
+func isLuck(s []ONE) (string) {
+	var aa []string
+	for i := 0; i < len(s); i++ {
+		rate,_ := strconv.Atoi(s[i].Rate)
+		for j := 0; j < rate; j++ {
+			aa = append(aa,s[i].Id)
+		}
+	}
+
+	aa = common.Random(aa,1)
+
+	return aa[0]
+
+}
+
 
 
 
--- src/route/Routes.go
+++ src/route/Routes.go
@@ -16,5 +16,11 @@ type Routes []Route
 
 var routes = Routes{
 	Route{Name:"UserList", Method:"GET",   Pattern:"/UserList",           HandlerFunc:controller.UserList},
-	Route{Name:"AddData", Method:"POST",   Pattern:"/AddData",           HandlerFunc:controller.AddData},
+	Route{Name:"GetWord", Method:"POST",   Pattern:"/GetWord",           HandlerFunc:controller.GetWord},
+	Route{Name:"GetWordZ", Method:"POST",   Pattern:"/GetWordZ",           HandlerFunc:controller.GetWordZ},
+	Route{Name:"IsPlay", Method:"POST",   Pattern:"/IsPlay",           HandlerFunc:controller.IsPlay},
+	Route{Name:"AllLuckList", Method:"POST",   Pattern:"/AllLuckList",           HandlerFunc:controller.AllLuckList},
+	Route{Name:"GetGameTimes", Method:"POST",   Pattern:"/GetGameTimes",           HandlerFunc:controller.GetGameTimes},
+	Route{Name:"Luck", Method:"POST",   Pattern:"/Luck",           HandlerFunc:controller.Luck},
+	Route{Name:"UpdateStatus", Method:"POST",   Pattern:"/UpdateStatus",           HandlerFunc:controller.UpdateStatus},
 }
